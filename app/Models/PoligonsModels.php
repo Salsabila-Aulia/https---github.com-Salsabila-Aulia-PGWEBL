c3f29550143dafd2236446e98a6748d6fe6c3c95
Salsabila-Aulia <salsabilaauliaalzzati2004>
2025-03-11 19:30:32 +0700

    first commit

---
+++ app/Models/PoligonsModels.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PoligonsModels extends Model
+{
+    protected $table = 'poligons';
+
+    protected $guarded = ['id'];
+}
---
+++ resources/views/table.blade.php
@@ -0,0 +1,41 @@
+@extends('layout.template')
+
+@section('content')
+<table class="table table-striped">
+    <th>
+        <tr>
+            <th>No</th>
+            <th>Nama</th>
+            <th>NIM</th>
+            <th>Kelas</th>
+        </tr>
+    </th>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>Salsa</td>
+            <td>520763</td>
+            <td>A</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Nasywa</td>
+            <td>514952</td>
+            <td>A</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Nia</td>
+            <td>516723</td>
+            <td>A</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Jenni</td>
+            <td></td>
+            <td>A</td>
+        </tr>
+    </tbody>
+</table>
+@endsection
+
