061bf673fc4524ed054fe4c3e576b3e621f28fc9
Salsabila-Aulia <salsabilaauliaalzzati2004>
2025-03-27 04:19:20 +0700

    update_pgwl_Aaara6

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PointsModel;
+use Illuminate\Http\Request;
+use App\Models\PolygonsModels;
+use App\Models\PolylinesModel;
+use App\Models\PolylinesModels;
+
+class ApiController extends Controller
+{
+    public function __construct()
+    {
+        $this->points = new PointsModel();
+        $this->polylines = new PolylinesModels();
+        $this->polygons = new PolygonsModels();
+
+    }
+
+    public function points()
+    {
+        $points = $this->points->geojson_points();
+
+        return response()->json($points);
+    }
+
+    public function polylines()
+    {
+        $polylines = $this->polylines->geojson_polylines();
+
+        return response()->json($polylines, 200, [], JSON_NUMERIC_CHECK);
+    }
+    public function polygons()
+    {
+        $polygons = $this->polygons->geojson_polygons();
+
+        return response()->json($polygons, 200, [], JSON_NUMERIC_CHECK);
+    }
+}
